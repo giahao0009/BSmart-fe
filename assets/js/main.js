ff6cfcb91c178e9dc6f8e34f38cca6cfe7130c89
nguyenhao0709 <giahao0009@gmail.com>
2022-12-31 01:20:13 +0700

    Mentor profile

--- assets/js/main.js
+++ assets/js/main.js
@@ -122,4 +122,44 @@ $(document).ready(function () {
     preloader.style.opacity = "0";
     preloader.style.visibility = "hidden";
   }, 500);
+
+  // Login with google demo
+  var googleUser = {};
+  var startApp = function () {
+    gapi.load("auth2", function () {
+      // Retrieve the singleton for the GoogleAuth library and set up the client.
+      auth2 = gapi.auth2.init({
+        client_id: "YOUR_CLIENT_ID.apps.googleusercontent.com",
+        cookiepolicy: "single_host_origin",
+        // Request scopes in addition to 'profile' and 'email'
+        //scope: 'additional_scope'
+      });
+      attachSignin(document.getElementById("customBtn"));
+    });
+  };
+  function attachSignin(element) {
+    console.log(element.id);
+    auth2.attachClickHandler(
+      element,
+      {},
+      function (googleUser) {
+        alert("Login with google successfully");
+      },
+      function (error) {
+        alert(JSON.stringify(error, undefined, 2));
+      }
+    );
+  }
+  startApp();
+
+  // Function openTabRegister
+  function openTabRegister(name) {
+    var i;
+    var x = document.getElementsByClassName("tab-item-register");
+    console.log(x);
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    document.getElementById(name).style.display = "block";
+  }
 });
